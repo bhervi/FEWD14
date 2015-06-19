6fddd40432f2b357d9fd4a541fd20087aa35e4e5
Bhervi_Patel <bhervip@gmail.com>
2015-06-18 18:54:11 -0700

    added slider function

---
+++ new slider/js/navbar.js
@@ -0,0 +1,15 @@
+Element.prototype.Navbar = function(){
+
+  var navbar = this;
+
+
+  this.createButton = function(){
+
+    btn.classList.add('button');
+
+    btn.addEventListener('mousedown',function(){
+
+      }
+    });
+  };
+}
