6c8a1b3daeac5a26a4c33bd526c4f66d05dfe255
Bhervi_Patel <bhervip@gmail.com>
2015-06-22 10:52:54 -0700

    added side nav and toggle

--- new slider/js/app.js
+++ new slider/js/app.js
@@ -1,2 +1,5 @@
 var sliderElement = document.getElementById('slider');
 sliderElement.Slider();
+
+var navbarElement = document.getElementById('sidemenu');
+navbarElement.Navbar();
--- new slider/js/navbar.js
+++ new slider/js/navbar.js
@@ -1,15 +1,17 @@
 Element.prototype.Navbar = function(){
 
   var navbar = this;
+  var container = navbar.parentElement;
+  var btn = navbar.querySelector('.toggle');
 
+  btn.addEventListener('click', function(ev){
 
-  this.createButton = function(){
+    if(container.className === 'active'){
+      container.className = '';
+    } else {
+      container.className = 'active'
+    }
+    
+  });
 
-    btn.classList.add('button');
-
-    btn.addEventListener('mousedown',function(){
-
-      }
-    });
-  };
 }
