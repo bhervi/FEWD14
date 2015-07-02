15bfac2614b2f918b1ee76c84f3c6cdbbd9572b9
Bhervi_Patel <bhervip@gmail.com>
2015-07-01 18:28:48 -0700

    adding search

--- gallery/js/search.js
+++ gallery/js/search.js
@@ -1,7 +1,49 @@
 Element.prototype.Search = function(){
-    var gallery = document.getElementById('gallery');
-    gallery.getFilterList()
+    var search = this,
+        gallery = document.getElementById('gallery');
+        gallery.getFilterList()
 // when the user focuses on the input, clear its contents
 // after the user presses 'enter/return', filter the gallery <li> using the tags from the JSON model.
+
+  document.getElementById('search').onkeydown = function activate(e, message)
+  {
+    if (isNotEnter(e))
+    {
+        return true;
+    }
+    else
+        if (message.length >= 1)
+        {
+            search();
+        }
+        return false;
+    }
+  console.log(onkeydown);
+
+this.connect = function(){
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "./models/popular-photos.json", true);
+    xhr.setRequestHeader("Content-Type", "application/json");
+
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState == 4) {
+          var response = JSON.parse(xhr.responseText);
+          tags = response.tags;
+          gallery.layoutPhotos();
+
+      }
+    }
+    xhr.send();
+  };
+
+  this.init = function() {
+    go.addEventListener('click', function(ev){
+        ev.preventDefault();
+        search.send();
+      });
+
+  };
+
+  this.init();
 
 };
